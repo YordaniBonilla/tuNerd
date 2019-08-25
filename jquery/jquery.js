5ed6da9ef6ab594d4ccb4fa66945a79e827dc52d
YordaniBonilla <b.yordani93@gmail.com>
2019-08-24 21:53:24 -0500

    integrate modals for each planet and integrate jquery for showing modals when clicking on image

---
+++ jquery/jquery.js
@@ -0,0 +1,31 @@
+$("#planet1").click(function (e) {
+    $("#modal1").modal('show');
+});
+
+$("#planet2").click(function (e) {
+    $("#modal2").modal('show');
+});
+
+$("#planet3").click(function (e) {
+    $("#modal3").modal('show');
+});
+
+$("#planet4").click(function (e) {
+    $("#modal4").modal('show');
+});
+
+$("#planet5").click(function (e) {
+    $("#modal5").modal('show');
+});
+
+$("#planet6").click(function (e) {
+    $("#modal6").modal('show');
+});
+
+$("#planet7").click(function (e) {
+    $("#modal7").modal('show');
+});
+
+$("#planet8").click(function (e) {
+    $("#modal8").modal('show');
+});

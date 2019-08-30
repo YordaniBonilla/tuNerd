eb73a7170191443f0970927cc122fc04b9d54b27
YordaniBonilla <b.yordani93@gmail.com>
2019-08-30 03:11:43 -0500

    subscribe button , table borders & background , magazine page, facebookshare

--- client/src/components/navbar/NavBar.jsx
+++ client/src/components/navbar/NavBar.jsx
@@ -5,7 +5,7 @@ function NavBar () {
     return (
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <a class="navbar-brand" href="#">
-                <img src="Images/letter-p.png" width="50" height="50" alt=""/>
+                <img src="Images/letter-p.png" width="40" height="40" alt=""/>
             </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                     aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -13,10 +13,11 @@ function NavBar () {
                 </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav mr-auto">
+                    <ul class="navbar-nav">
                         <li class="nav-item active">
                             <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
                         </li>
+                        <li>
                     <div class="dropdown">
                         <button type="button" class="btn  dropdown-toggle" data-toggle="dropdown">
                             Explore
@@ -24,11 +25,16 @@ function NavBar () {
                         <div class="dropdown-menu">
                             <a class="dropdown-item" href="/planetCalculator.html">Planet Weight</a>
                             <a class="dropdown-item" href="moons.html">Moons</a>
-                            <a class="dropdown-item" href="#">Dwarfs</a>
+                            <a class="dropdown-item" href="magazines.html">Magazines</a>
                         </div>
+                        
                     </div>
+                    </li>
                     </ul>
-                   
+                    <div class="ml-auto">
+                      <button type="button" class="btn btn-info  btn-sm mr-2">Login</button>    
+                      <button type="button" class="btn btn-info  btn-sm">Sign Out</button>
+                    </div>
         </div>
     </nav>
     );
